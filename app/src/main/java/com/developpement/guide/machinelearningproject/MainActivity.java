c63512f98892bd199b367f07b3cbb775e1cf8e0e
GULJINDER Karan <KGULJINDER@fr.scc.com>
2019-01-17 10:42:24 +0100

    Ajout du Paint sur l'activity

--- app/src/main/java/com/developpement/guide/machinelearningproject/MainActivity.java
+++ app/src/main/java/com/developpement/guide/machinelearningproject/MainActivity.java
@@ -1,13 +1,28 @@
 package com.developpement.guide.machinelearningproject;
 
-import android.support.v7.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.developpement.guide.machinelearningproject.View.PaintView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private PaintView paintView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintView = (PaintView) findViewById(R.id.paintView);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
     }
+
 }
